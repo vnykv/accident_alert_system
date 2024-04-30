2d8e78ffe8600df0461845f4f98c9508788fe3ad
vinayak p k <99945298+vnykv@users.noreply.github.com>
2024-04-30 23:18:17 +0530

    Add files via upload

--- detection.py
+++ detection.py
@@ -6,39 +6,28 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from twilio.rest import Client
 import env_vars as ev
+from email.message import EmailMessage
+import ssl
+import smtplib
+import time
+import threading
+import sys
 
 # Load the trained YOLO model
-model = YOLO("ML part/best.pt")
+model = YOLO("ML_part/best.pt")
 
-def send_whatsapp_message(to_whatsapp_number, message):
-    # Function to send WhatsApp message using Twilio API
-    account_sid = ev.account_sid
-    auth_token = ev.auth_token
-    from_whatsapp_number = ev.from_whatsapp_number
-
-    client = Client(account_sid, auth_token)
-
-    try:
-        # Send WhatsApp message
-        message = client.messages.create(
-            body=message,
-            from_="whatsapp:" + from_whatsapp_number,
-            to="whatsapp:" + to_whatsapp_number,
-        )
-        print("WhatsApp message sent successfully:", message.sid)
-        return True
-
-    except Exception as e:
-        print("Error sending WhatsApp message:", str(e))
-        return False
+# Variable to track the last time an email was sent
+last_email_sent_time = 0
 
 def send_email_with_frame(frame_path, class_name, rounded_conf, location):
+    global last_email_sent_time
+
     try:
         # Function to send email with an attached image
-        msg = MIMEMultipart()
-        msg["From"] = ev.from_email
-        msg["To"] = ev.to_email
-        msg["Subject"] = "Accident Detected"
+        em = EmailMessage()
+        em['From'] = ev.email_s
+        em['To'] = ev.email_r
+        em['subject'] = ev.subject
 
         # Create HTML content with formatting
         body = f"""
@@ -48,29 +37,39 @@ def send_email_with_frame(frame_path, class_name, rounded_conf, location):
             </body>
         </html>
         """
+        em.set_content(body)
 
-        msg.attach(MIMEText(body, 'html'))
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(ev.email_s,ev.email_p)
+            smtp.sendmail(ev.email_s, ev.email_r, em.as_string())
+        
+        # Print confirmation message
+        print("Email sent!")
 
-        with open(frame_path, "rb") as img_file:
-            image = MIMEImage(img_file.read())
-            msg.attach(image)
+        # Update the last email sent time
+        last_email_sent_time = time.time()
 
-        # Send email using SMTP
-        smtp_server = "smtp.gmail.com"
-        smtp_port = 587
-        smtp_username = ev.smtp_username
-        smtp_password = ev.smtp_password
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(ev.from_email, ev.to_email, msg.as_string())
-        server.quit()
-
-        return True  # Return success flag
+        return True
 
     except Exception as e:
         print("Error sending email:", str(e))
         return False
+
+def email_sending_thread(frame_path, class_name, rounded_conf, location):
+    global last_email_sent_time
+
+    # Check if 20 seconds have passed since the last email was sent
+    elapsed_time = time.time() - last_email_sent_time
+    if elapsed_time < 20:
+        # If less than 20 seconds have passed, wait for the remaining time
+        time.sleep(20 - elapsed_time)
+
+    # Send email
+    if send_email_with_frame(frame_path, class_name, rounded_conf, location):
+        print("Email sent!")
+    else:
+        print("Failed to send email")
 
 class Detection:
     detection_result = []
@@ -87,7 +86,7 @@ class Detection:
                 conf = round(box.conf[0].item(), 2)
                 print("Confidence Score:", conf)  # Print confidence score
 
-                if conf >= 0.5:  # Adjust the confidence threshold as needed
+                if conf >= 0.5 and class_id == 1 :  # Adjust the confidence threshold as needed
                     print("Object type:", class_id)
                     print("Coordinates:", cords)
                     print("Confidence Score:", conf)
@@ -97,12 +96,9 @@ class Detection:
                     class_name = "*MODERATE*" if class_id == 0 else "*SEVERE*"
                     rounded_conf = str(round(conf * 100, 2))
 
-                    # Send email notification
-                    if send_email_with_frame(image_path, class_name, rounded_conf, location):
-                        print("Email sent!")
-
-                    message = f"Accident detected of class { class_name } at location { location }. Severity level: { rounded_conf }%. Check email for image."
-                    send_whatsapp_message(ev.to_whatsapp_number, message)
+                    # Start a separate thread to send email
+                    email_thread = threading.Thread(target=email_sending_thread, args=(image_path, class_name, rounded_conf, location))
+                    email_thread.start()
 
                     # Reinitialize the result list to empty to get updated values for the next detection
                     Detection.detection_result = []
@@ -116,16 +112,17 @@ class Detection:
     @staticmethod
     def static_detection():
         # Function to perform static image detection
-        image_path = "ML part/temp/temp.jpg"
+        image_path = "ML_part/temp/temp.jpg"
         Detection.detection_result = Detection.prediction(image_path)
         return Detection.detection_result
 
     @staticmethod
     def video_stream_detection():
+        file_name = sys.argv[1]
         # Function to perform video stream detection
         frame_width, frame_height = 1280, 720
         #stream_url = "http://195.196.36.242/mjpg/video.mjpg"
-        stream_url="ML part/inputs/videos/video3.mp4"
+        stream_url="ML_part/inputs/uploads/"+file_name
         cap = cv2.VideoCapture(stream_url)
 
         while cap.isOpened():
@@ -138,7 +135,7 @@ class Detection:
             resized_frame = cv2.resize(frame, (frame_width, frame_height))
 
             # Save the resized frame as an image in a temporary directory
-            temp_image_path = "ML part/temp/temp.jpg"
+            temp_image_path = "ML_part/temp/temp.jpg"
             cv2.imwrite(temp_image_path, resized_frame)
 
             # Perform object detection on the image and show the results
